53bc57730d3c29293dc9367d09c3c05c274147b7
Jaanus <jaanus.kaap@gmail.com>
2016-08-25 13:41:59 +0300

    Skip no-code stuff in some executables

--- 0.IDA/GenCoverageInfo.py
+++ 0.IDA/GenCoverageInfo.py
@@ -11,6 +11,7 @@ if not os.path.exists(baseDir + "KavalAntsBB"):
 
 filename = idaapi.get_root_filename().lower()
 base = idaapi.get_imagebase()
+allBlocks = {}
 BBcount = 0
 Fcount = 0
 
@@ -25,13 +26,15 @@ for segment_ea in Segments():
 		Fcount += 1
 		blocks = idaapi.FlowChart(idaapi.get_func(location))
 		for block in blocks:
-			#print "BB at " + hex(block.startEA)
-			#print " MEMORY OFFSET:" + hex(block.startEA - base)
-			#print " FILE OFFSET: " + hex(idaapi.get_fileregion_offset(block.startEA))
-			#print " ORIGINAL VALUE: " + hex(idaapi.get_byte(block.startEA))
 			BBcount += 1
-			line = "%08X|%08X|%02X" % ((block.startEA - base), (idaapi.get_fileregion_offset(block.startEA)), (idaapi.get_byte(block.startEA)))
-			file.write("\n" + line)
+			if block.startEA not in allBlocks:
+				if GetMnem(block.startEA) == "":
+					print "Skipping %08X because this is not code" % (block.startEA)
+					print "    " + GetDisasm(block.startEA)
+					break
+				line = "%08X|%08X|%02X" % ((block.startEA - base), (idaapi.get_fileregion_offset(block.startEA)), (idaapi.get_byte(block.startEA)))
+				file.write("\n" + line)
+				allBlocks[block.startEA] = True
 file.close()
 print "Discovered %d basic blocks in %d functions" % (BBcount, Fcount)
 qexit()
